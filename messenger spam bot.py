602d1d787ceb460911dcfd79c670be54e35af530
Pouya Tavakoli <69327223+pouyatavakoli@users.noreply.github.com>
2020-11-23 15:47:25 +0330

    Add files via upload

---
+++ messenger spam bot.py
@@ -0,0 +1,27 @@
+# Note : this code was writtenn for educational perposes only
+# you have to install "pyautogui" using  this command  :
+# pip install pyautogui
+
+# importing part 
+import pyautogui
+import time
+# main part
+count = 0
+pyautogui.click(10,5)
+# do for ever :)
+while True :
+
+    # creating a kill switch
+    pyautogui.FAILSAFE = True
+    # kill switch is putting the mouse cursor in the top left corner of screen :)
+
+    # you can write your favorite massage here 
+    pyautogui.write(f"spam bot test")
+
+    count = 1  
+    # send massage 
+    pyautogui.press("ENTER")
+    # wait for site to response 
+    time.sleep(1)
+# go to https://web.whatsapp.com/ or your sightly messenger web version or app select the chat and run this code 
+# make sure that you set the messenger to send massage by hitting Enter
